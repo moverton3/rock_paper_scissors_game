a60320c3a61522edc377578a9c571659dd9d9c90
Mallory Overton <malloryoverton3@gmail.com>
2017-11-15 22:55:49 -0700

    added to the js and html to get game logic to work

--- rps.js
+++ rps.js
@@ -1,35 +1,37 @@
-// RPS Game Logic
 
-var score = 0;
+function activate(playerChoice) {
+  var options = ["Paper", "Scissors", "Rock"]
+  var computerChoice = options[Math.floor(Math.random() * 3)];
+  console.log("You chose " + playerChoice)
+  console.log("Computer chose " + computerChoice)
+// decide who and how player or computer wins
+    if ( playerChoice === computerChoice ) {
+      document.getElementById("decision").innerHTML = "Whoops! Looks like a tie."
+    }
+    else if ( playerChoice === "Rock" && computerChoice === "Scissors" ||
+         playerChoice === "Paper" && computerChoice === "Rock" ||
+         playerChoice === "Scissors" && computerChoice === "Paper") {
+      document.getElementById("decision").innerHTML = "You won!"
+      document.getElementById("gameoutcome").innerHTML = ("You chose: " + playerChoice + " Computer chose: " + computerChoice)
+    }
+    else {
+      document.getElementById("decision").innerHTML = "You lost!"
+      document.getElementById("gameoutcome").innerHTML = ("You chose: " + playerChoice + " Computer chose: " + computerChoice)
+    }
 
-var userChoice;
-
-var options = {
-  "0": "Paper",
-  "1": "Scissors",
-  "2": "Rock"
-};
-
-var oppChoice = {
-  init: function() {
-    this.store = Math.floor(Math.random() * 3);
-    this.display = options[this.store];
-  },
-  display: " ",
-  store: " "
 }
+//
+var paperButton = document.getElementById('paperButton')
+paperButton.addEventListener('click', function() {
+  activate("Paper")
+})
 
-var pickWinner = function(player, computer) {
-// numbers are equal to options variable at top
-  var order [0, 1, 2, 0];
-  if ( player === computer ) { return "Whoops! Looks like a tie." }
-  if order[player] === order[computer + 1]) {
-    score++;
-    return "You win!";
-  } else {
-    score--;
-    return "You lose."
-  }
-}
+var scissorButton = document.getElementById('scissorButton')
+scissorButton.addEventListener('click', function() {
+  activate("Scissors")
+})
 
-console.log("oppChoice": oppChoice);
+var rockButton = document.getElementById('rockButton')
+rockButton.addEventListener('click', function() {
+  activate("Rock")
+})
